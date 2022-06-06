5fc07b98fd9af794e0eb5222bb8cf0b29c3d7720
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-06-06 15:47:28 +0200

    primo codice wip

---
+++ classes/Movie.php
@@ -0,0 +1,26 @@
+<?php
+class Movie{
+  protected $titolo;
+  protected $regista;
+  protected $attori;
+  protected $trama;
+  protected $locandina;
+
+  function __constructor($_titolo, $_regista, $_attori){
+    $this->titolo = $_titolo;
+    $this->regista = $_regista;
+    $this->attori = $_attori;
+  }
+  public function getTitolo(){
+    return $this->titolo;
+  }
+  public function getRegista(){
+    return $this->regista;
+  }
+  public function getAttori(){
+    return $this->attori;
+  }
+
+  }
+
+?>
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+require_once __DIR__."/classes/Movie.php";
+
+$movies = [
+  [
+    'titolo' => 'Film1',
+    'regista' => 'regista1',
+    'attori' => ['attore1', 'attore2', 'attore3'],
+    'trama' => 'lorem',
+    'locandina' => 'locandina',
+  ],
+  [
+    'titolo' => 'Film2',
+    'regista' => 'regista2',
+    'attori' => ['attore1', 'attore2', 'attore3'],
+    'trama' => 'lorem',
+    'locandina' => 'locandina',
+  ],
+  [
+    'titolo' => 'Film3',
+    'regista' => 'regista3',
+    'attori' => ['attore1', 'attore2', 'attore3'],
+    'trama' => 'lorem',
+    'locandina' => 'locandina',
+  ],
+  [
+    'titolo' => 'Film4',
+    'regista' => 'regista4',
+    'attori' => ['attore1', 'attore2', 'attore3'],
+    'trama' => 'lorem',
+    'locandina' => 'locandina',
+  ],
+  [
+    'titolo' => 'Film5',
+    'regista' => 'regista5',
+    'attori' => ['attore1', 'attore2', 'attore3'],
+    'trama' => 'lorem',
+    'locandina' => 'locandina',
+  ]
+  ];
+$Movies = [];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php - OOP-1</title>
+</head>
+<body>
+  <h1>Lista Films</h1>
+  <?php
+    foreach($movies as $key => $value){
+      $movie = new Movie($value['titolo'], $value['regista'], $value['attori']);
+  ?>
+     <!-- <h3>Film: <?php echo "{$movie->getTitolo()} - {$movie->getRegista()} - {$movie->getAttori()}" ?> </h3> -->
+     <h3>Titolo: <?php echo "{$movie->getTitolo()}" ?> </h3>
+     <h3>Regista: <?php echo "{$movie->getRegista()}" ?> </h3>
+     <h3>Attori: <?php echo "{$movie->getAttori()}" ?> </h3>
+  <?php
+  }
+  ?>
+  
+</body>
+</html>
